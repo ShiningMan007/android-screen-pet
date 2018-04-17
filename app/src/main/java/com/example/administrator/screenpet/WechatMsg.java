1eee6b5393795521b176f29b6b7d53f160bdcc4e
shiningman007 <2275642346@qq.com>
2018-04-17 23:24:57 +0800

    Main Framework

---
+++ app/src/main/java/com/example/administrator/screenpet/WechatMsg.java
@@ -0,0 +1,4 @@
+package com.example.administrator.screenpet;
+
+public interface WechatMsg {
+}
---
+++ app/src/main/java/com/example/administrator/screenpet/WechatNotifyService.java
@@ -0,0 +1,36 @@
+package com.example.administrator.screenpet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class NotifyService extends NotificationListenerService {
+    public static final String SEND_WX_BROADCAST="SEND_WX_BROADCAST";
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn, RankingMap rankingMap) {
+        String packageName=sbn.getPackageName();
+        String message = sbn.getNotification().tickerText.toString();
+        Intent intent=new Intent();
+        intent.setAction(SEND_WX_BROADCAST);
+        Bundle bundle=new Bundle();
+        bundle.putString("packageName",packageName);
+        bundle.putString("message",message);
+        intent.putExtras(bundle);
+        this.sendBroadcast(intent);
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn, RankingMap rankingMap) {
+    }
+}
